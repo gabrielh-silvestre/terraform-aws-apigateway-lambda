4bceefa31bee22e18cdb295b06530cfd977ea545
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-12-08 18:03:41 -0300

    chore: add VPC configuration example for creating a new VPC

--- example/main.tf
+++ example/main.tf
@@ -17,6 +17,12 @@ module "api-gateway-with-lambda" {
   account_id = data.aws_caller_identity.current.account_id
   region     = data.aws_region.current.name
 
+  # --------- Only if you want to create a new VPC ---------
+  # vpc_cidr_block             = "10.0.0.0/16"
+  # public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
+  # private_subnet_cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24"]
+  # security_group_name        = "example-security-group"
+
   default_tags = {
     "Project" = "terraform-aws-lambda-apigateway"
   }
